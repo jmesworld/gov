9baffc1d421cc6e50eac12116cda28f070aa70cf
mayoreee <mayoreee1@gmail.com>
2023-06-25 11:15:23 +0200

    fix: initial render glitch

--- features/Onboarding/components/MobileViewDisabled.tsx
+++ features/Onboarding/components/MobileViewDisabled.tsx
@@ -21,7 +21,15 @@ const MobileViewDisabled = () => {
   }
 
   return (
-    <Box p={4} bg="lilac" color="purple" height="100vh" display="flex" alignItems="center" justifyContent="center">
+    <Box
+      p={4}
+      bg="lilac"
+      color="purple"
+      height="100vh"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+    >
       <Text fontSize="md">
         Please use the web view for the best experience.
       </Text>
