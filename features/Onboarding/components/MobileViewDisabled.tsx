7b39d0b9b0f5d4c5739a6f685efbaece777adda9
mayoreee <mayoreee1@gmail.com>
2023-06-25 11:15:13 +0200

    fix: initial render glitch

--- features/Onboarding/components/MobileViewDisabled.tsx
+++ features/Onboarding/components/MobileViewDisabled.tsx
@@ -1,22 +1,27 @@
+import { useState, useEffect } from "react";
 import { useBreakpointValue, Box, Text } from "@chakra-ui/react";
 
 const MobileViewDisabled = () => {
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsReady(true);
+    }, 500); // Adjust the delay time as needed
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
   const isMobileView = useBreakpointValue({ base: true, md: false });
 
-  if (!isMobileView) {
-    return null; // Render nothing if not in mobile view
+  if (!isReady || !isMobileView) {
+    return null; // Render nothing until the component is ready or not in mobile view
   }
 
   return (
-    <Box
-      p={4}
-      bg="lilac"
-      color="purple"
-      height={"100vh"}
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-    >
+    <Box p={4} bg="lilac" color="purple" height="100vh" display="flex" alignItems="center" justifyContent="center">
       <Text fontSize="md">
         Please use the web view for the best experience.
       </Text>
