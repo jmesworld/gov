fb10e01f6308997457cb926b6acbc46c92a4e579
Henok <=>
2023-07-19 10:14:50 +0300

    fix: fix the threshold issue

--- lib/calculateVotes.ts
+++ lib/calculateVotes.ts
@@ -9,9 +9,8 @@ export const calculateVotes = ({
 }) => {
   const coinYes = (Number(coin_Yes) || 0) / Math.pow(10, 6);
   const coinNo = (Number(coin_no) || 0) / Math.pow(10, 6);
-
   // TODO: fix the threshold calculation
-  const threshold = totalSupply * 0.1 + (coinNo > 0 ? coinYes - coinNo : 0);
+  const threshold = totalSupply * 0.1 + coinNo;
   const thresholdPercent = (threshold / totalSupply) * 100;
   const yesPercentage = (totalSupply > 0 ? coinYes / totalSupply : 0) * 100;
   const noPercentage = (totalSupply > 0 ? coinNo / totalSupply : 0) * 100;
