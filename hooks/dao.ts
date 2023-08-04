20f1b3cab539bdd439b6111d6a9e4a5191f61a61
Henok <=>
2023-08-04 17:34:27 +0300

    fix: remove votes on directors on public proposals

--- hooks/dao.ts
+++ hooks/dao.ts
@@ -1,9 +1,6 @@
 import { useMemo } from 'react';
 import { useCosmWasmClientContext } from '../contexts/CosmWasmClient';
-import {
-  useDaoMultisigListVotersQuery,
-  useDaoMultisigListVotesQuery,
-} from '../client/DaoMultisig.react-query';
+import { useDaoMultisigListVotersQuery } from '../client/DaoMultisig.react-query';
 import { DaoMultisigQueryClient } from '../client/DaoMultisig.client';
 import { useDaoMultisigConfigQuery } from '../client/DaoMultisig.react-query';
 
@@ -16,37 +13,27 @@ export const useDaoMemberList = (id?: string, proposalId?: number) => {
     return new DaoMultisigQueryClient(cosmWasmClient, id as string);
   }, [cosmWasmClient, id, proposalId]);
 
-  const votesQuery = useDaoMultisigListVotesQuery({
-    client: daoQueryClient ?? undefined,
-    args: { proposalId: proposalId! },
-    options: { refetchInterval: 10000, enabled: proposalId !== undefined },
-  });
   const daoMembers = useDaoMultisigListVotersQuery({
     client: daoQueryClient ?? undefined,
     args: {
       limit: 1000,
     },
     options: {
-      enabled: votesQuery.data !== undefined,
+      enabled: true,
     },
   });
 
-  const memberVoted = useMemo(() => {
-    return votesQuery?.data?.votes ?? [];
-  }, [votesQuery?.data?.votes]);
   const voters = useMemo(() => {
     const members = daoMembers?.data?.voters ?? [];
     return members
       ?.sort((a, b) => b.weight - a.weight)
       ?.map(member => {
-        const vote = memberVoted.find(vote => vote.voter === member.addr);
         return {
           ...member,
           voter: member.addr,
-          vote: vote?.vote as string,
         };
       });
-  }, [daoMembers?.data?.voters, memberVoted]);
+  }, [daoMembers?.data?.voters]);
 
   return {
     daoQueryClient,
